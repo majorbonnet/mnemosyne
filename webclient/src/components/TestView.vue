4c933c888a3bb6735f1ed460bf3ccd4d2cbcaf82
Ben Jaspers <bmjaspers@gmail.com>
2025-04-11 16:21:05 -0400

    Add webclient to docker-compose and add log out button

--- webclient/src/components/TestView.vue
+++ webclient/src/components/TestView.vue
@@ -5,8 +5,10 @@ const authStore = useAuthStore();
 
 <template>
   <h1>{{ authStore.user.username }}</h1>
-  <h1>{{ authStore.user.token }}</h1>
-  <h1>{{ authStore.user.refToken }}</h1>
+  <h3>{{ authStore.user.token }}</h3>
+  <h3>{{ authStore.user.refToken }}</h3>
+
+  <button @click="authStore.logout()">Log out</button>
 </template>
 
 <style scoped>
